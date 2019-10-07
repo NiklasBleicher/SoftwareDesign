4497fb0df619e559118227453bbd51327bc62284
Niklas Bleicher <niklas.bleicher@hs-furtwangen.de>
2019-10-07 22:26:58 +0200

    Started Working on C# Tasks, Linked Task Folder in Workspace

---
+++ Aufgabe1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            uint x; //32-bit -> 4 Byte
+            ushort y; //16bit -> 2 Byte
+            long z; //64bit -> 8 Byte
+
+            string test = "Hello, World"; //12 Characters * 16-Bit = 192 byte = 24 Byte 
+            
+            Console.WriteLine(test.Length);
+            Console.Read();
+        }
+    }
+}
