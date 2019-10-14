76ca2157d138cbd2d9b4b7d8586eaa46fafb712b
Niklas Bleicher <niklas.bleicher@hs-furtwangen.de>
2019-10-15 01:25:32 +0200

    Added LetterReverse Method/Initialized other Methods

--- Aufgabe1/Dreher.cs
+++ Aufgabe1/Dreher.cs
@@ -1,22 +1,53 @@
  public static class LetterSwitcher
-    {
+ {
+     
         public static void Main(string[] args)
         {
-            Console.WriteLine("Bitte einen kleinen Satz eingeben");
+            Console.WriteLine("Enter a small sentence:");
             Console.Write("> ");
             var text = Console.ReadLine();
             string letters = reverseLetters(text);
-            /*string words = reverseWords(text);
-            string sentence = reverseSentence(text);*/
+            string words = reverseWords(text);
+            string sentence = reverseSentence(text);
             Console.WriteLine(letters);
             Console.Read();
 
         }
 
+        public static string Reverse(string text)
+        {
+            char[] charArray = text.ToCharArray();
+            Array.Reverse(charArray);
+            string word = charArray.ToString();
+            string reversedString = string.Join( " ", charArray);
+
+            return reversedString;
+        }
+
         private static string reverseLetters(string text)
         {
-            string newText = "This is a Test Text!";
+            string[] sentence = text.Split(' ');
+            string[] newSentence = new string[sentence.Length];
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                newSentence[i] = Reverse(sentence[i]);
+            }
+
+            string finalSentence = string.Join(" ", newSentence);
             
-            return newText;
+            return finalSentence;
         }
-    }
+
+        private static string reverseWords(string text)
+        {
+
+            return "Test";
+        }
+
+        private static string reverseSentence(string text)
+        {
+            //string words = reverseLetters(text);
+            return "Test";
+        }
+}
