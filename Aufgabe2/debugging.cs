a4230a3be39ed210b194805ee65e512fa0ee6017
Niklas Bleicher <niklas.bleicher@hs-furtwangen.de>
2019-10-16 00:28:42 +0200

    Initialized Person Array

--- Aufgabe2/debugging.cs
+++ Aufgabe2/debugging.cs
@@ -7,22 +7,36 @@
             
             //Safe Number from 1-10 in intArray
             int[] intArray = new int[10];
-            for(int i = 1; i <= intArray.Length; i++)
+            for(int i = 1; i < intArray.Length; i++)
             {
                 intArray[i] = i;
-                WriteLine(intArray[i]);
+                //WriteLine(intArray[i]);
             }
             
             //.toString()
             foreach (var number in intArray)
             {
                 string stringNumber = number.ToString();
-                WriteLine(stringNumber);
+                //WriteLine(stringNumber);
             }
+            Person[] personArray = new Person[5];
             
-            //Initialize new Person
+            //Initialize new Person and add it to Person Array
             Person person1 = new Person() {FirstName = "Hans", LastName = "Hoch", Age = new DateTime(1988, 4, 12)};
-            
+            personArray[0] = person1;
+            Person person2 = new Person() {FirstName = "Niklas", LastName = "Bleicher", Age = new DateTime(1999, 5, 8)};
+            personArray[1] = person2;
+
+            for(int i = 0; i < personArray.Length; i++)
+            {
+                if (personArray[i].Age > personArray[i + 1].Age)
+                {
+                    Person oldest = personArray[i];
+                    WriteLine($"The oldest Person is: {oldest.FirstName}");
+                }
+                
+            }
+            Console.WriteLine(person1);
             Console.Read();
         }
     }
